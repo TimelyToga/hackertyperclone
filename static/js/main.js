984778d70184ce076e8495deaecef0c4328a427a
TimelyToga <tblumberg39@gmail.com>
2015-10-04 17:39:28 -0400

    Better Appending scheme

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,35 @@
+var varsInUse = [];
+var tabs = 0;
+var lines = [];
+var linePointer = 0;
+var jumpDistance = 3;
+var curP;
+
+var defaultCodeLine = "MDASH = (i / 2);";
+
 window.onload=function () {
     $(document).keypress(function() {
-        var new_p = document.createElement("p");
-        new_p.innerHTML = "for (int i; ) { }";
-        $("#view_wrapper").append(new_p);
+        curP = document.createElement("p");
+        curP.innerHTML = defaultCodeLine;
+        revealCode();
         window.scrollBy(0,50);
     });
 };
+
+function revealCode(){
+    if(lines.length == 0 || ((lines.size != 0) && ((lines[0].length - linePointer) <= jumpDistance))){
+        // Create more
+        generateCode();
+    }
+
+    $("p").last().append(" some shit ");
+
+}
+
+function generateCode(){
+   lines.push(defaultCodeLine);
+}
+
+function generateLine(){
+
+}
