a021fc7a0db6edc754dd9840c086507fd5dc1d36
Timothy Blumberg <timothy.blumberg@skyflow.com>
2020-11-19 11:09:45 -0800

    Setup redirect

--- hackertyperclone/views.py
+++ hackertyperclone/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import RequestContext, loader
+from django.shortcuts import redirect
 
 
 def main_view(request):
-    template = get_template('main.html')
-    html = template.render(request)
-    return HttpResponse(html)
+    # Just redirect to my personal site
+    return redirect("https://timothyblumberg.com/hacker_typer")
